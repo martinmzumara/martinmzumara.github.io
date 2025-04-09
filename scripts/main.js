a868599cf57959f467577722b03bee8564c36901
martinmzumara <martinmzumara08@gmail.com>
2025-04-09 16:16:44 +0200

    Restructure for GitHub Pages

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+let lastScrollTop = 0;
+const header = document.querySelector('header');
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (scrollTop > lastScrollTop) {
+        // Scrolling down
+        header.style.transform = 'translateY(-100%)';
+    } else {
+        // Scrolling up
+        header.style.transform = 'translateY(0)';
+    }
+    
+    lastScrollTop = scrollTop;
+});
